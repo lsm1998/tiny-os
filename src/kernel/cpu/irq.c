433ee7bfe073bcf37d202e10e3d0b85de4417d7b
lsm1998 <487005831@qq.com>
2026-03-14 01:27:12 +0800

    irq_install

--- src/kernel/cpu/irq.c
+++ src/kernel/cpu/irq.c
@@ -3,31 +3,47 @@
 #include "comm/cpu_instr.h"
 #include "os_cfg.h"
 
-#define IDT_TABLE_SIZE 128
-
 static gate_desc_t idt_table[IDT_TABLE_SIZE];
 
-void exception_handler(void);
+void exception_handler_unknown(void);
 
-static void do_default_handler(const char* msg)
+static void do_handler_default(exception_frame_t* frame, const char* msg)
 {
     for (;;)
     {
     }
 }
 
-void do_exception_handler(exception_frame_t* frame)
+void do_handler_unknown(exception_frame_t* frame)
 {
-    do_default_handler("unknown exception");
+    do_handler_default(frame, "unknown exception");
+}
+
+void do_handler_divide_error(exception_frame_t* frame)
+{
+    do_handler_default(frame, "divide exception");
 }
 
 void irq_init(void)
 {
     for (int i = 0; i < IDT_TABLE_SIZE; i++)
     {
-        gate_desc_set(&idt_table[i], (uint32_t)exception_handler, KERNEL_SELECTOR_CS,
+        gate_desc_set(&idt_table[i], (uint32_t)exception_handler_unknown, KERNEL_SELECTOR_CS,
                       GATE_P_PRESENT | GATE_DPL0 | GATE_TYPE_INT);
     }
 
+    irq_install(IRQ0_DE, do_handler_divide_error);
+
     lidt((uint32_t)idt_table, sizeof(idt_table));
+}
+
+int irq_install(int irq_num, irq_handler_t handler)
+{
+    if(irq_num < 0 || irq_num >= IDT_TABLE_SIZE)
+    {
+        return -1;
+    }
+    gate_desc_set(&idt_table[irq_num], (uint32_t)handler, KERNEL_SELECTOR_CS,
+                  GATE_P_PRESENT | GATE_DPL0 | GATE_TYPE_INT);
+    return 0;
 }
--- src/kernel/include/cpu/irq.h
+++ src/kernel/include/cpu/irq.h
@@ -3,6 +3,10 @@
 
 #include "comm/types.h"
 
+#define IDT_TABLE_SIZE 128
+
+#define IRQ0_DE 0
+
 typedef struct exception_frame_t
 {
     uint32_t gs, fs, es, ds;
@@ -10,8 +14,14 @@ typedef struct exception_frame_t
     uint32_t eip, cs, eflags;
 } exception_frame_t;
 
+typedef void (*irq_handler_t)(exception_frame_t* frame);
+
 void irq_init(void);
 
-void do_exception_handler(exception_frame_t* frame);
+void do_handler_unknown(exception_frame_t* frame);
+
+void do_handler_divide_error(exception_frame_t* frame);
+
+int irq_install(int irq_num, irq_handler_t handler);
 
 #endif // __IRQ_H__
